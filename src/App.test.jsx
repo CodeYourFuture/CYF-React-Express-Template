08686961d9176e9c5733c42d78479a0a80aba9d7
Sally McGrath <sally@codeyourfuture.io>
2023-12-16 13:12:34 +0000

    provision mini app

---
+++ src/App.test.jsx
@@ -0,0 +1,16 @@
+import { describe, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
+
+describe("App", () => {
+  it("renders a title", () => {
+    //arrange
+    render(<App />);
+    //act
+    //assert
+    expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+      "Vite React Express CYF"
+    );
+  });
+});
