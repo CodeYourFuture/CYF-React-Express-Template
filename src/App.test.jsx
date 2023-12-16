a81df611aca095f72bd0e80d6c8cfb1878259518
Sally McGrath <sally@codeyourfuture.io>
2023-12-16 13:33:44 +0000

    add a second test and rm globals
    We confused ourselves

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,3 @@
-import { describe, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 
 import App from "./App";
@@ -12,5 +11,13 @@ describe("App", () => {
     expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
       "Vite React Express CYF"
     );
+  });
+
+  it("renders a counter", () => {
+    // arrange
+    render(<App />);
+    // act
+    //assert
+    expect(screen.getByRole("button")).toHaveTextContent("count is 0");
   });
 });
